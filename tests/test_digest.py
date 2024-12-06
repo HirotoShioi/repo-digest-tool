2374be8a2d4d08a7ec05d4ed878b06b7abe82f6a
Hiroto Shioi <shioihigg@gmail.com>
2024-12-06 21:53:53 +0900

    remove unused

--- tests/test_digest.py
+++ tests/test_digest.py
@@ -1,36 +1,21 @@
-import pytest
-import tempfile
 from pathlib import Path
 from repo_tool import generate_digest, download_repo
 from repo_tool.core.repository import calculate_repo_id
 from repo_tool.core.contants import DIGEST_DIR
 
-MOCK_REPO_URL = "https://github.com/HirotoShioi/repo-digest-tool"
-MOCK_PROMPT = None  # 必要ならカスタムプロンプトを設定
-MAX_DIGEST_SIZE_MB = 1 * 1024 * 1024  # ダイジェストファイルの最大サイズ（MB）
-MIN_DIGEST_SIZE = 150 * 1024  # ダイジェストファイルの最小サイズ（バイト）
+REPO_URL = "https://github.com/HirotoShioi/repo-digest-tool"
+PROMPT = None
 
 
-@pytest.fixture
-def temp_repo_dir():
-    """一時的なディレクトリを作成して返す"""
-    with tempfile.TemporaryDirectory() as temp_dir:
-        yield Path(temp_dir)
-
-
-def test_digest_file_generation(temp_repo_dir):
-    # 一時的なリポジトリIDとパス
-    repo_id = calculate_repo_id(MOCK_REPO_URL)
+def test_digest_file_generation():
+    repo_id = calculate_repo_id(REPO_URL)
     digest_path = Path(DIGEST_DIR) / f"{repo_id}.txt"
 
-    # ダウンロードとダイジェスト生成の呼び出し
-    download_repo(MOCK_REPO_URL, repo_id, branch=None)
-    generate_digest(repo_id, MOCK_PROMPT)
+    download_repo(REPO_URL, repo_id, branch=None)
+    generate_digest(repo_id, PROMPT)
 
-    # ダイジェストファイルが生成されたことを確認
     assert digest_path.exists(), "Digest file should be generated."
 
-    # ダイジェストファイルの先頭部分を確認
     with digest_path.open("r", encoding="utf-8") as f:
         first_line = f.readline().strip()
         assert (
@@ -38,8 +23,9 @@ def test_digest_file_generation(temp_repo_dir):
             == "The following text represents the contents of the repository."
         )
 
-    # ファイルサイズの確認（MB単位）
     file_size = digest_path.stat().st_size
+    MIN_DIGEST_SIZE = 150 * 1024
+    MAX_DIGEST_SIZE_MB = 1 * 1024 * 1024
     assert file_size > MIN_DIGEST_SIZE, "Digest file size should be larger than 200KB."
     assert (
         file_size <= MAX_DIGEST_SIZE_MB
